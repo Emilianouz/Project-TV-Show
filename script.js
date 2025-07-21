3e950cc0f0a2a51cd2b8d243891d89bc5824c148
Emilianouz <135679131+Emilianouz@users.noreply.github.com>
2025-07-21 22:33:37 +0100

    html to create template in Index file. Scripts created to show all episodes displaying:  The name of the episode, combining season and episode number, the medium-sized image for the episode and the summary text of the episode CSS styles to show the card for each episode.

--- script.js
+++ script.js
@@ -9,4 +9,36 @@ function makePageForEpisodes(episodeList) {
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
+
 window.onload = setup;
+function createEpisodeCard(episode){
+  const template = document.getElementById("episode-card");
+  const card = template.content.cloneNode(true);
+  const episodeFormatted = formatEpisodeNumber(episode.season,episode.number)
+  card.querySelector("h3").textContent = episode.name + ' - ' + episodeFormatted;
+ // card.querySelector("a").href = episode.url;
+  card.querySelector("p").innerHTML = episode.summary;
+  card.querySelector("img").src = episode.image.medium;
+  card.querySelector("img").alt = episode.image.name;
+  return card;
+}
+
+function formatEpisodeNumber(season, number) {
+  const seasonFormatted = String(season).padStart(2, '0');
+  const episodeFormatted = String(number).padStart(2, '0');
+  return `S${seasonFormatted}E${episodeFormatted}`;
+}
+
+function formatTime(timeSecs) {
+  let minutes = Math.floor(timeSecs / 60);
+  let seconds = timeSecs % 60;
+  return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+}
+
+const episodeCard = getAllEpisodes().map(createEpisodeCard);
+//document.getElementById("root").append(...episodeCard);
+document.body.append(...episodeCard);
+
+//getAllEpisodes()
+//getOneEpisode()
+
